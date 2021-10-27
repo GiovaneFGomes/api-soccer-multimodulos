9df674679af62e2d00e2016727e4b35dc05df828
Giovane <giovanefernandes35@gmail.com>
2021-10-27 10:43:59 -0300

    Removing dependencie Jackson

--- repository/src/main/java/com/giovane/soccer/model/entity/Team.java
+++ repository/src/main/java/com/giovane/soccer/model/entity/Team.java
@@ -1,14 +1,7 @@
 package com.giovane.soccer.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 import javax.persistence.*;
-import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -30,10 +23,5 @@ public class Team {
 
     @Column(name = "name_country")
     private String country;
-
-//    @JsonDeserialize(using = LocalDateDeserializer.class)
-//    @JsonSerialize(using = LocalDateSerializer.class)
-//    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "UTC")
-//    private LocalDate date;
 
 }
