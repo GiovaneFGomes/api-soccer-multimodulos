ed743b5352e56923d477b9f03d2df0e63037104a
Giovane <giovanefernandes35@gmail.com>
2021-11-19 09:56:28 -0300

    Add spring cloud config dependencie

--- src/main/java/com/giovane/soccer/SoccerApplication.java
+++ src/main/java/com/giovane/soccer/SoccerApplication.java
@@ -2,8 +2,11 @@ package com.giovane.soccer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+// anotacao para executar o servidor em uma application Spring Boot
+@EnableConfigServer
 public class SoccerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SoccerApplication.class, args);
