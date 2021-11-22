431033b2e8f36077f313545df5965c3885faf4a8
Giovane <giovanefernandes35@gmail.com>
2021-11-22 10:50:53 -0300

    Refactoring name src -> config-server

--- config-server/main/java/com/giovane/soccer/SoccerApplication.java
+++ config-server/main/java/com/giovane/soccer/SoccerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 // anotacao para executar o servidor em uma application Spring Boot
-//@EnableConfigServer
+@EnableConfigServer
+@SpringBootApplication
 public class SoccerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SoccerApplication.class, args);
